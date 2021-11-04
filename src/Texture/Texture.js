6c64a7d2c54d445bac2ce0e81229c206ef6a0a35
Miao Zhuang <1060950782@163.com>
2021-11-04 14:09:12 +0800

    fix canvas push

--- src/Texture/Texture.js
+++ src/Texture/Texture.js
@@ -7,7 +7,6 @@ class Texture {
         this.miniGL = miniGL;
         this.canvas = document.createElement('canvas');
         this.ctx = this.canvas.getContext('2d');
-        document.body.appendChild(this.canvas);
     }
     lg2(n) {
 		return Math.log(n) / Math.log(2);
