ded97602025dc6e2fd5ce5f7edbab23edb09de40
mizy <1060950782@163.com>
2022-05-07 15:40:37 +0800

    fix ts

--- src/dragonBones/MiniGLTextureAtlasData.ts
+++ src/dragonBones/MiniGLTextureAtlasData.ts
@@ -1,65 +1,64 @@
 import Texture from '../Texture/Texture';
-//<reference path="../../src/dragonBones/DragonBones.d.ts" />
-class MiniGLTextureAtlasData extends dragonBones.TextureAtlasData {
+//<reference path="../../src/dragonBones/DragonBones.d.ts" /> 
+class MiniGLTextureAtlasData extends (window.dragonBones ? dragonBones.TextureAtlasData : null) {
+  scale = 1;
+  disposeEnabled: boolean;
+  private _renderTexture: any;
+  _onClear() {
+    super._onClear();
 
-    scale = 1;
-    disposeEnabled: boolean;
-    private _renderTexture: any;
-    _onClear() {
-        super._onClear();
-
-        if (this.disposeEnabled && this._renderTexture !== null) {
-            this._renderTexture.dispose();
-        }
-
-        this.disposeEnabled = false;
-        this._renderTexture = null;
+    if (this.disposeEnabled && this._renderTexture !== null) {
+      this._renderTexture.dispose();
     }
 
-    createTexture() {
-        const texture = dragonBones.BaseObject.borrowObject(MiniTextureData);
-        return texture;
-    }
-    setRenderTexture(texture, miniGL) {
-        this._renderTexture = texture;
+    this.disposeEnabled = false;
+    this._renderTexture = null;
+  }
 
-        if (this._renderTexture !== null) {
-            for (let k in this.textures) {
-                const textureData = this.textures[k];
+  createTexture() {
+    const texture = dragonBones.BaseObject.borrowObject(MiniTextureData);
+    return texture;
+  }
+  setRenderTexture(texture, miniGL) {
+    this._renderTexture = texture;
 
-                const renderTexture = new Texture(miniGL);
-                renderTexture.create({ image: texture, rect: textureData.region, reverseY: false, name: textureData.name });
-                (textureData as any).renderTexture = renderTexture;
-            }
-        } else {
-            for (let k in this.textures) {
-                const textureData = this.textures[k];
-                (textureData as any).renderTexture = null;
-            }
-        }
-        return texture;
+    if (this._renderTexture !== null) {
+      for (let k in this.textures) {
+        const textureData = this.textures[k];
+
+        const renderTexture = new Texture(miniGL);
+        renderTexture.create({ image: texture, rect: textureData.region, reverseY: false, name: textureData.name });
+        (textureData as any).renderTexture = renderTexture;
+      }
+    } else {
+      for (let k in this.textures) {
+        const textureData = this.textures[k];
+        (textureData as any).renderTexture = null;
+      }
     }
+    return texture;
+  }
 
 }
 
 /**
  * @internal
  */
-class MiniTextureData extends dragonBones.TextureData {
-    renderTexture = null; // Initial value.
-    _onClear() {
-        super._onClear.call(this);
-        if (this.renderTexture !== null) {
-            this.renderTexture.destroy(false);
-        }
-        this.renderTexture = null;
+class MiniTextureData extends (window.dragonBones ? dragonBones.TextureData : null) {
+  renderTexture = null; // Initial value.
+  _onClear() {
+    super._onClear.call(this);
+    if (this.renderTexture !== null) {
+      this.renderTexture.destroy(false);
     }
+    this.renderTexture = null;
+  }
 }
 
 MiniTextureData.toString = function () {
-    return '[class dragonBones.MiniTextureData]';
+  return '[class dragonBones.MiniTextureData]';
 };
 MiniGLTextureAtlasData.toString = function () {
-    return '[class dragonBones.MiniAtlasData]';
+  return '[class dragonBones.MiniAtlasData]';
 };
 export default MiniGLTextureAtlasData;
--- src/dragonBones/index.ts
+++ src/dragonBones/index.ts
@@ -4,96 +4,95 @@ import MiniGLArmatureDisplay from "./MiniGLArmatureDisplay";
 import MiniGL from "../MiniGL";
 import Image from "../Mesh/Image";
 
-const { BaseObject, Armature } = dragonBones;
-class DragonBoneObject extends dragonBones.BaseFactory {
-    miniGL: MiniGL;
-    display: MiniGLArmatureDisplay;
-    _dragonBonesInstance: dragonBones.DragonBones;
-    constructor(miniGL) {
-        super();
-        this.miniGL = miniGL;
-        this.getDragonBonesInstance();
-        this.display = new MiniGLArmatureDisplay();
+class DragonBoneObject extends (window.dragonBones ? dragonBones.BaseFactory : null) {
+  miniGL: MiniGL;
+  display: MiniGLArmatureDisplay;
+  _dragonBonesInstance: dragonBones.DragonBones;
+  constructor(miniGL) {
+    super();
+    this.miniGL = miniGL;
+    this.getDragonBonesInstance();
+    this.display = new MiniGLArmatureDisplay();
+  }
+
+  addFrameEvent() {
+    this.miniGL.on("beforerender", this.update);
+  }
+  update = (delta) => {
+    this._dragonBonesInstance.advanceTime(delta * 0.001);
+  };
+
+  getDragonBonesInstance = () => {
+    if (!this._dragonBonesInstance) {
+      this._dragonBonesInstance = new dragonBones.DragonBones({
+        hasDBEventListener: (event) => {
+          return this.miniGL._listeners[event];
+        },
+        // 转发事件
+        dispatchDBEvent: (event, data) => {
+          this.miniGL.fire(event, data);
+        },
+        addDBEventListener: (type, func) => { },
+        removeDBEventListener: (type, func) => { },
+      });
+      this.addFrameEvent();
+    }
+    this._dragonBonesInstance = this._dragonBonesInstance;
+  };
+
+  // 构建对象
+  buildArmatureDisplay(
+    armatureName,
+    dragonBonesName = "",
+    skinName = "",
+    textureAtlasName = ""
+  ) {
+    const armature = this.buildArmature(
+      armatureName,
+      dragonBonesName,
+      skinName,
+      textureAtlasName
+    );
+    this._dragonBonesInstance.clock.add(armature);
+    return armature.display;
+  }
+
+  /**
+   * @override 复现方法
+   * @param {*} dataPackage
+   */
+  _buildArmature(dataPackage) {
+    const armature = dragonBones.BaseObject.borrowObject(dragonBones.Armature);
+    const armatureDisplay = new MiniGLArmatureDisplay({
+      miniGL: this.miniGL,
+    });
+    armature.init(
+      dataPackage.armature,
+      armatureDisplay,
+      armatureDisplay,
+      this._dragonBones
+    );
+
+    return armature;
+  }
+
+  _buildSlot(_dataPackage, slotData, armature) {
+    const slot = dragonBones.BaseObject.borrowObject(MiniGLSlot);
+    const sprite = new Image();
+    slot.init(slotData, armature, sprite, sprite);
+    return slot;
+  }
+
+  _buildTextureAtlasData(textureAtlasData, textureAtlas) {
+    if (textureAtlasData) {
+      textureAtlasData.setRenderTexture(textureAtlas, this.miniGL);
+    } else {
+      textureAtlasData = dragonBones.BaseObject.borrowObject(MiniGLTextureAtlasData);
     }
 
-    addFrameEvent() {
-        this.miniGL.on("beforerender", this.update);
-    }
-    update = (delta) => {
-        this._dragonBonesInstance.advanceTime(delta * 0.001);
-    };
+    return textureAtlasData;
+  }
 
-    getDragonBonesInstance = () => {
-        if (!this._dragonBonesInstance) {
-            this._dragonBonesInstance = new dragonBones.DragonBones({
-                hasDBEventListener: (event) => {
-                    return this.miniGL._listeners[event];
-                },
-                // 转发事件
-                dispatchDBEvent: (event, data) => {
-                    this.miniGL.fire(event, data);
-                },
-                addDBEventListener: (type, func) => { },
-                removeDBEventListener: (type, func) => { },
-            });
-            this.addFrameEvent();
-        }
-        this._dragonBonesInstance = this._dragonBonesInstance;
-    };
-
-    // 构建对象
-    buildArmatureDisplay(
-        armatureName,
-        dragonBonesName = "",
-        skinName = "",
-        textureAtlasName = ""
-    ) {
-        const armature = this.buildArmature(
-            armatureName,
-            dragonBonesName,
-            skinName,
-            textureAtlasName
-        );
-        this._dragonBonesInstance.clock.add(armature);
-        return armature.display;
-    }
-
-    /**
-     * @override 复现方法
-     * @param {*} dataPackage
-     */
-    _buildArmature(dataPackage) {
-        const armature = dragonBones.BaseObject.borrowObject(Armature);
-        const armatureDisplay = new MiniGLArmatureDisplay({
-            miniGL: this.miniGL,
-        });
-        armature.init(
-            dataPackage.armature,
-            armatureDisplay,
-            armatureDisplay,
-            this._dragonBones
-        );
-
-        return armature;
-    }
-
-    _buildSlot(_dataPackage, slotData, armature) {
-        const slot = BaseObject.borrowObject(MiniGLSlot);
-        const sprite = new Image();
-        slot.init(slotData, armature, sprite, sprite);
-        return slot;
-    }
-
-    _buildTextureAtlasData(textureAtlasData, textureAtlas) {
-        if (textureAtlasData) {
-            textureAtlasData.setRenderTexture(textureAtlas, this.miniGL);
-        } else {
-            textureAtlasData = BaseObject.borrowObject(MiniGLTextureAtlasData);
-        }
-
-        return textureAtlasData;
-    }
-
-    destroy() { }
+  destroy() { }
 }
 export default DragonBoneObject;
